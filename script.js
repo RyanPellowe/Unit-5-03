a7681fcd32990a72ee0e1c898bfb4b584896e04d
RyanPellowe <ryan.pellowe@ocsbstudent.ca>
2021-02-11 15:47:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("button").addEventListener("click", buttonOn);
+
+function buttonOn() {
+  alert("Hey!")
+  document.getElementById("backround").style.backgroundColor = "red" 
+}
+
+document.getElementById("paragraph").innerHTML = "Click some buttons and see what happens!";
+
+document.getElementById("ocean predator").innerHTML = "heres a random picture of a shark for literally no reason.";
